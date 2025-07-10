04cc1bce3146600694ce2c2a8ba645c58dc5a787
pooja <poojadm1519@gmail.com>
2025-07-10 12:54:19 -0500

    Updated file

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,14 +19,31 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
-console.log("The result is: " + result);
+let result = "5" - 2; 
+//implicit type conversion
+console.log("The implicit conversion result is: " + result);
+let explicitConversionResult = Number("5") - 2; 
+console.log("The explicit conversion result is: " + explicitConversionResult);
 
-let isValid = Boolean("false");
+let isValid = false;
+//In Boolean everything, including non-empty strings, non-zero numbers, objects, and arrays, will be converted to true. So, the string "false" is being evaluated to true which is not valid
+//In order to fix it we need to remove explict type conversion and quotes. 
 if (isValid) {
     console.log("This is valid!");
 }
 
 let age = "25";
-let totalAge = age + 5;
-console.log("Total Age: " + totalAge);
+let totalAge = Number(age) + 5;
+console.log("Total Age "  + totalAge);
+
+//Examples for implicit and explicit are below
+
+let totalCost = "5" * 10;
+console.log("Implicit type converted total cost " +totalCost);
+
+let mathScore = null;
+let scienceScore = 60;
+console.log("Before Implicit type converted overall score " + mathScore + scienceScore);
+overAllScore = Number(mathScore) + 60;
+console.log("After Implicit type converted overall score " +overAllScore);
+
